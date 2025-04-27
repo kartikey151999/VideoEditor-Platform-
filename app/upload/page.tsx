820a8ac6b5b03e376ca4b8c6d295fc123967de0d
Prerna Pande <160504210+prerna-pande@users.noreply.github.com>
2025-04-27 15:28:34 +0530

    creating a page.tsx file to upload page

---
+++ app/upload/page.tsx
@@ -0,0 +1,10 @@
+import UploadZone from '@/components/UploadZone'
+
+export default function UploadPage() {
+  return (
+    <main className="p-6">
+      <h1 className="text-2xl font-bold mb-4">Upload Your Video</h1>
+      <UploadZone />
+    </main>
+  )
+}
